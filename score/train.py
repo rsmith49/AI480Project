7887b66463e8afc7bcf1b600169a2ad382b18167
Ryan Smith <ryans_cais@yahoo.com>
2016-12-07 22:56:15 -0800

    Fixed october issue

--- score/train.py
+++ score/train.py
@@ -172,7 +172,8 @@ if __name__ == '__main__':
     dates = []
     conn = Connection()
     conn.connect()
-    sqlquery = 'SELECT DISTINCT `date` FROM player_daily_2014 WHERE datenum > 735700'
+    sqlquery = "SELECT DISTINCT `date` FROM player_daily_2014 WHERE datenum > 735700 AND " + \
+               "NOT `date` LIKE 'OCT%'"
     datestrs = conn.query(sqlquery)
     conn.close()
     dates = [parse(datestr[0] + ' 2014') for datestr in datestrs]
